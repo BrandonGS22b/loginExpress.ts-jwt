470c92f7ec38a86d0de4700ba18cedbc0c767571
BRANDON_GARCIA <159663607+BrandonGS22b@users.noreply.github.com>
2024-09-14 01:50:26 -0500

    update

--- src/mongo/conexionbd.ts
+++ src/mongo/conexionbd.ts
@@ -3,18 +3,14 @@ import mongoose from 'mongoose';
 export class MongoDatabase {
 
   static async connect(): Promise<boolean> {
-    // URL de conexión a MongoDB Atlas
-    const mongoUrl = 'mongodb+srv://brandong:a9AJ8RKP1CrGRjm5@brandong.sawk0.mongodb.net/?retryWrites=true&w=majority';
-    const dbName = 'brandong';
+    // URL de conexión simplificada
+    const mongoUrl = 'mongodb+srv://brandong:a9AJ8RKP1CrGRjm5@brandong.sawk0.mongodb.net/brandong?retryWrites=true&w=majority';
 
     // Habilitar logs detallados de Mongoose para depuración
     mongoose.set('debug', true);
 
     try {
-      await mongoose.connect(mongoUrl, {
-        dbName: dbName,  // Nombre de la base de datos
-        tlsAllowInvalidCertificates: true, // Permitir certificados no válidos (considerar eliminar en producción)
-      });
+      await mongoose.connect(mongoUrl);
 
       console.log('Conectado a MongoDB Atlas');
       return true;
