c7a353f4a31c0e795e10dc414024d987539928b4
BRANDON_GARCIA <159663607+BrandonGS22b@users.noreply.github.com>
2024-09-13 21:52:57 -0500

    update

--- src/mongo/conexionbd.ts
+++ src/mongo/conexionbd.ts
@@ -3,8 +3,9 @@ import mongoose from 'mongoose';
 export class MongoDatabase {
 
   static async connect(): Promise<boolean> {
-    const mongoUrl = 'mongodb+srv://UTS:uts2024@uts.ccyqodk.mongodb.net/Dev2024E191?retryWrites=true&w=majority&appName=UTS/Usuario';
-    const dbName = 'Usuario';
+    const mongoUrl = 'mongodb+srv://brandon222b:i4XpuZK7HCbZ6I1N@cluster0.fn3zw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/mystore';
+    //mongodb+srv://UTS:uts2024@uts.ccyqodk.mongodb.net/Dev2024E191?retryWrites=true&w=majority&appName=UTS/Usuario
+    const dbName = 'mystore';
 
     try {
       await mongoose.connect(mongoUrl, {
