ac529274457e67991d314258b56f25b207fab640
BRANDON_GARCIA <159663607+BrandonGS22b@users.noreply.github.com>
2024-09-25 21:27:25 -0500

    se actualiza el numero de salts o de espacios que va tener el token para agilizar en  react la respuesta

--- src/config/bcrypt.adapter.ts
+++ src/config/bcrypt.adapter.ts
@@ -1,16 +1,15 @@
 import { compareSync, genSaltSync, hashSync } from 'bcryptjs';
 
-
-
 export const bcryptAdapter = {
 
   hash: (password: string) => {
-    const salt = genSaltSync();
-    return hashSync(password, salt)
+    const saltRounds = 7; // Número de rondas más bajo para hacer el proceso más rápido
+    const salt = genSaltSync(saltRounds); // Pasar el número de rondas aquí
+    return hashSync(password, salt);
   },
 
-  compare: (password:string, hashed: string) => {
+  compare: (password: string, hashed: string) => {
     return compareSync(password, hashed);
   }
 
-}
+};
