1fb5655f62fa89013f12462fde1d7f35ce8f6fc1
Brandon_Garcia <brandondulian@hotmail.com>
2024-09-29 10:35:22 -0500

    update

--- src/auth/register-user.dto.ts
+++ src/auth/register-user.dto.ts
@@ -1,28 +1,28 @@
 import { regularExps } from '../config/';
 
-
-
-
 export class RegisterUserDto {
-
   private constructor(
     public name: string,
     public email: string,
     public password: string,
+    public role: string // Añadir role aquí
   ) {}
 
-  static create( object: { [key:string]:any } ): [string?, RegisterUserDto?] {
-    const { name, email, password } = object;
+  static create(object: { [key: string]: any }): [string?, RegisterUserDto?] {
+    const { name, email, password, role } = object; // Incluir role en la desestructuración
 
-    if ( !name ) return ['Missing name'];
-    if ( !email ) return ['Missing email'];
-    if ( !regularExps.email.test( email ) ) return ['Email is not valid'];
-    if ( !password ) return ['Missing password'];
-    if ( password.length < 6 ) return ['Password too short'];
+    if (!name) return ['Missing name'];
+    if (!/^[a-zA-Z\s]+$/.test(name)) return ['Name contains invalid characters'];
 
-    return [undefined, new RegisterUserDto(name, email, password)];
+    if (!email) return ['Missing email'];
+    if (!regularExps.email.test(email)) return ['Email is not valid'];
 
+    if (!password) return ['Missing password'];
+    if (password.length < 6) return ['Password too short'];
+
+    if (!role) return ['Missing role']; // Validar que el rol esté presente
+    if (!['usuario', 'admin', 'tecnico'].includes(role)) return ['Role must be one of: usuario, admin, tecnico']; // Validar que el rol sea uno de los permitidos
+
+    return [undefined, new RegisterUserDto(name, email, password, role)];
   }
-
-
 }
