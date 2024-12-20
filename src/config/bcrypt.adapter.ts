bf73f77f81b7512c338960344692f4671e6d8060
Brandon_Garcia <159663607+BrandonGS22b@users.noreply.github.com>
2024-12-20 08:36:17 -0500

    Update bcrypt.adapter.ts

--- src/config/bcrypt.adapter.ts
+++ src/config/bcrypt.adapter.ts
@@ -3,7 +3,7 @@ import { compareSync, genSaltSync, hashSync } from 'bcryptjs';
 export const bcryptAdapter = {
 
   hash: (password: string) => {
-    const saltRounds = 5; // Número de rondas más bajo para hacer el proceso más rápido
+    const saltRounds = 3; // Número de rondas más bajo para hacer el proceso más rápido
     const salt = genSaltSync(saltRounds); // Pasar el número de rondas aquí
     return hashSync(password, salt);
   },
