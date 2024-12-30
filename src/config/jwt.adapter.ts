87e99e8d49351a0174a7a2f6a80f4922b1bdc407
Brandon_Garcia <159663607+BrandonGS22b@users.noreply.github.com>
2024-12-30 01:24:42 -0500

    Update jwt.adapter.ts

--- src/config/jwt.adapter.ts
+++ src/config/jwt.adapter.ts
@@ -1,4 +1,4 @@
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import { envs } from './envs';
 
 const JWT_SEED = envs.JWT_SEED;
@@ -6,20 +6,29 @@ const JWT_SEED = envs.JWT_SEED;
 export class JwtAdapter {
 
   // Generación del token de manera síncrona
-  static generateToken(payload: any, duration: string = '2m'): string | null {
+  static generateToken(payload: Record<string, any>, duration: string = '2m'): string {
     try {
+      // Verifica que la duración del token sea válida
+      const isValidDuration = /^(\d+)([smhdw])$/.test(duration);
+      if (!isValidDuration) {
+        throw new Error("Duración de token inválida");
+      }
+      
       return jwt.sign(payload, JWT_SEED, { expiresIn: duration });
     } catch (err) {
-      return null; // Si hay un error, devuelve null
+      console.error("Error generando el token:", err);
+      throw new Error('Error generando el token'); // Lanzamos un error en lugar de devolver null
     }
   }
 
   // Validación del token de manera síncrona
-  static validateToken<T>(token: string): T | null {
+  static validateToken<T>(token: string): T {
     try {
-      return jwt.verify(token, JWT_SEED) as T;
+      const decoded = jwt.verify(token, JWT_SEED);
+      return decoded as T;
     } catch (err) {
-      return null; // Si hay un error, devuelve null
+      console.error("Token no válido:", err);
+      throw new Error('Token no válido'); // Lanzamos un error en lugar de devolver null
     }
   }
 }
