ab9a3f783194ba7ed05629cd383b0999dd849a3d
Brandon_Garcia <159663607+BrandonGS22b@users.noreply.github.com>
2025-01-07 03:40:09 -0500

    se eliminan componente inservibles

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,12 @@ import { Router } from 'express';
 import Authroutes from './presentation/controller/authLogin/routes'; // Apunta al router del controller
 
 // Importar las rutas faltantes
-import categoriaProblemaRoutes from './presentation/controller/categoriaproblema/router';
+
 import mantenimientoRoutes from './presentation/controller/mantenimiento/router';
-import historialMantenimientoRoutes from './presentation/controller/historiasmantenimiento/router';
+
 import asignacionTecnicoRoutes from './presentation/controller/asignacionTecnico/router';
 import GestionMantenimiento from './presentation/controller/GestionMantenimiento/router';
-import evaluacionRoutes from './presentation/controller/evaluacion/router';
+
 import solicitud from './presentation/controller/solicitud/router';
 import GestionTecnicosRoutes from './presentation/controller/gestiontecnico/routes';
 
@@ -24,12 +24,10 @@ export class AppRoutes {
     router.use('/api/GestionTecnico',GestionTecnicosRoutes );
     // Rutas para cada uno de los modeloss
     router.use('/api/solicitud', solicitud);
-    router.use('/api/categoriaProblema', categoriaProblemaRoutes);
     router.use('/api/mantenimiento', mantenimientoRoutes);
-    router.use('/api/historialMantenimiento', historialMantenimientoRoutes);
     router.use('/api/asignacionTecnico', asignacionTecnicoRoutes);
     router.use('/api/GestionMantenimiento', GestionMantenimiento);
-    router.use('/api/evaluacion', evaluacionRoutes);
+
     
 
   
