77675d88adb496a172830b721a48b8f9422c9b41
Brandon_Garcia <brandondulian@hotmail.com>
2025-01-30 17:58:53 -0500

    update cors para firebase

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -27,7 +27,7 @@ export class Server {
   async start() {
     // Middlewares
     this.app.use(cors({
-      origin: ['http://localhost:3000', 'http://192.168.1.5:3000'],
+      origin: ['http://localhost:3000', 'http://192.168.1.5:3000','https://proyecto-f7ec6.web.app'],
       credentials: true,
     }));
 
